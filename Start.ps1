15261f4aee18f3ae92686ab6ec4242667a364414
AlexeyDunin <DuninAlexey@gmail.com>
2019-07-14 22:16:29 +0300

    Bug fixes

--- Start.ps1
+++ Start.ps1
@@ -20,35 +20,37 @@
     }
 }
 
-function Test-Port
-{
-    param
-    (
-        $Address,
-        $Port
-    )
-    $tcpClient = new-object Net.Sockets.TcpClient
-    try
-    {
-        $tcpClient.Connect("$Address", $Port)
-        $true
-    }
-    catch
-    {
-        $false
-    }
-    finally
-    {
-        $tcpClient.Dispose()
-    }
-}
-
 Start-Job -Name "launch browser" -ScriptBlock {
+    function Test-Port
+    {
+        param
+        (
+            $Address,
+            $Port
+        )
+
+        $tcpClient = new-object Net.Sockets.TcpClient
+        try
+        {
+            $tcpClient.Connect("$Address", $Port)
+            $true
+        }
+        catch
+        {
+            $false
+        }
+        finally
+        {
+            $tcpClient.Dispose()
+        }
+    }
+
     do{
 
-        Start-Sleep -Seconds 5
-        Start-Process "http://localhost:5000"
+        Start-Sleep -Seconds 5       
     }while(-not (Test-Port -Address localhost -Port 5000))
+    
+    Start-Process "http://localhost:5000"
 }
 
 Start-Application $PSScriptRoot
