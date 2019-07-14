0e5408963f30b0b95ba649db478bfb068151c8ad
Adunin <duninalexey@gmail.com>
2019-07-14 12:51:07 +0000

    Launch browser async

--- Start.ps1
+++ Start.ps1
@@ -15,14 +15,40 @@
 
     foreach -Parallel ($project in $projectsDir)
     {
-        Write-Output "Run project $project"
-        if($project -eq "WebApp") { Start-Process "http://localhost:5000" }
-        
+        Write-Output "Run project $project"        
         dotnet run --project $(Join-Path $Using:scriptPath $project) --force
     }
 }
 
+function Test-Port
+{
+    param
+    (
+        $Address,
+        $Port
+    )
+    $tcpClient = new-object Net.Sockets.TcpClient
+    try
+    {
+        $tcpClient.Connect("$Address", $Port)
+        $true
+    }
+    catch
+    {
+        $false
+    }
+    finally
+    {
+        $tcpClient.Dispose()
+    }
+}
+
+Start-Job -Name "launch browser" -ScriptBlock {
+    do{
+
+        Start-Sleep -Seconds 5
+        Start-Process "http://localhost:5000"
+    }while(-not (Test-Port -Address localhost -Port 5000))
+}
+
 Start-Application $PSScriptRoot
-
-
-
